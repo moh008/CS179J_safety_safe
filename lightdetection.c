25254df914ed2c1453fded6bd5b726b83179100c
Abel Theodros <43688013+abeltheo001@users.noreply.github.com>
2020-07-17 12:46:05 -0600

    Update lightdetection.c comments

--- lightdetection.c
+++ lightdetection.c
@@ -1,12 +1,10 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
+/*
+* Safety Safe - Light Detection (prototype)
+* (awaiting testing)
+*
+* Author: Abel Theodros
+*/
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
@@ -20,7 +18,6 @@ void ACD_init()
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-//	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
@@ -28,7 +25,7 @@ int main(void) {
 	ACD_init();
     while (1) {
 	unsigned short x = ADC;
-	if (x>= MAX/2)
+	if (x>= 2*MAX/3)
 		PORTB = 0x01;
 	else
 		PORTB = 0x00;
