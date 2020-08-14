e4b112a57b6cd316910d33d9314f36adf525e517
moh008 <moh008@ucr.edu>
2020-08-14 00:11:05 -0700

    EEPROM Update
    Keep EEPROM storing 5 memory space
    
    It keeps showing history until access button pressed.
    When you press "next" button, it will show the 5 records in order of the latest history.

--- lcd_eeprom.ino
+++ lcd_eeprom.ino
@@ -6,16 +6,17 @@ LiquidCrystal lcd(8, 2, 4, 5, 6, 7);
 
 byte accessPin = 10;
 byte recordPin = A3;
+byte nextPin = A4;
 
 enum SwitchModes {PULLUP, PULLDOWN};
 SwitchModes switchMode = PULLUP;
 
-byte timestamp;
+byte timestamp, access, recordcheck, next, i;
 
-  
 void loop(){
-  byte access = digitalRead(accessPin);
-  byte recordcheck = digitalRead(recordPin);
+  access = digitalRead(accessPin);
+  recordcheck = digitalRead(recordPin);
+  
   if(access == HIGH && recordcheck == HIGH)
   {
     delay(100);
@@ -24,7 +25,8 @@ void loop(){
   else if(access == LOW)
   {
     timestamp = millis()/1000;
-    EEPROM.write(0, timestamp);
+    EEPROM.write(i, timestamp);
+    
     lcd.clear();
     lcd.print("Accessed");
     delay(1000);
@@ -33,21 +35,58 @@ void loop(){
     lcd.print("Press button");
     lcd.setCursor(0, 1);
     lcd.print("Access | Record");
+    if(i >= 4)
+    {
+      i = 0;
+    }
+    else
+    {
+      i = i + 1;
+    }
   }
   
   else if(recordcheck == LOW)
   {
     lcd.clear();
     lcd.print("Time accessed");
-    lcd.print(EEPROM.read(0)/3600);
+    lcd.setCursor(0, 1);
+    lcd.print(EEPROM.read(i-1)/3600);
     lcd.print("Hr ");
-    lcd.print((EEPROM.read(0)%3600/60);
+    lcd.print((EEPROM.read(i-1)%3600)/60);
     lcd.print("Min ");
-    lcd.print(EEPROM.read(0)%60);
+    lcd.print(EEPROM.read(i-1)%60);
     lcd.print("Sec");
-    
-    delay(2000);
-    
+       
+    while(recordcheck == LOW)
+    {
+      access = digitalRead(accessPin);
+      if(access == LOW)
+      {
+        break;
+      }
+      next = digitalRead(nextPin);
+      if(next == LOW)
+      {
+        if(i != 0)
+        {
+          i = i - 1;
+        }
+        else if(i == 0)
+        {
+          i = 4;
+        }
+        lcd.clear();
+    	lcd.print("Time accessed");
+    	lcd.setCursor(0, 1);
+    	lcd.print(EEPROM.read(i)/3600);
+    	lcd.print("Hr ");
+    	lcd.print((EEPROM.read(i)%3600)/60);
+    	lcd.print("Min ");
+    	lcd.print(EEPROM.read(i)%60);
+    	lcd.print("Sec");
+        delay(300);
+      }
+    }
     lcd.clear();
     lcd.print("Press button");
     lcd.setCursor(0, 1);
@@ -61,11 +100,13 @@ void setup()
   {
     pinMode(accessPin, INPUT);
     pinMode(recordPin, INPUT);
+    pinMode(nextPin, INPUT);
   }
   else
   {
     pinMode(accessPin, INPUT_PULLUP);
     pinMode(recordPin, INPUT_PULLUP);
+    pinMode(nextPin, INPUT_PULLUP);
   }
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -73,5 +114,5 @@ void setup()
   lcd.print("Press button");
   lcd.setCursor(0, 1);
   lcd.print("Access | Record");
-  
+  i = 0;
 }
